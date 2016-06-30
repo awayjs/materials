b1f09570f87726c6eda79f390f9982f219445b54
rob-bateman <rob@infiniteturtles.co.uk>
2016-06-30 05:04:44 +0100

    move .ts files back to modules, duh

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as data from "./lib/data";
-import * as methods from "./lib/methods";
-import * as surfaces from "./lib/surfaces";
-import { MethodMaterial } from "./lib/MethodMaterial";
-import { MethodMaterialMode } from "./lib/MethodMaterialMode";
-export { data, methods, surfaces, MethodMaterial, MethodMaterialMode };
