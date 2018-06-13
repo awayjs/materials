2cd166176c5191ef31732f54b8d1ff8b7c0022b6
Rob Bateman <rob@infiniteturtles.co.uk>
2018-06-13 13:33:25 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.log("AwayJS - Materials - 0.4.13");
+console.log("AwayJS - Materials - 0.4.14");
 
 export {LightEvent} from "./lib/events/LightEvent";
 export {TextureProjectorEvent} from "./lib/events/TextureProjectorEvent";
