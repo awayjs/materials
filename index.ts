bc24014616e251f32999d68eef775735f8417033
80prozent <80prozent@differentdesign.de>
2019-03-12 17:40:57 +0100

    change console.log to console.debug for the version log

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.log("AwayJS - Materials - 0.4.16");
+console.debug("AwayJS - Materials - 0.4.16");
 
 export {LightEvent} from "./lib/events/LightEvent";
 export {TextureProjectorEvent} from "./lib/events/TextureProjectorEvent";
