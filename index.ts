f32cf03aaecfd82934d6150eda272f053a43cdab
80prozent <80prozent@differentdesign.de>
2021-04-13 17:38:15 +0200

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.debug("AwayJS - Materials - 0.6.7");
+console.debug("AwayJS - Materials - 0.6.8");
 
 export {LightEvent} from "./lib/events/LightEvent";
 export {TextureProjectorEvent} from "./lib/events/TextureProjectorEvent";
